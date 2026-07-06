50e42a0de31eba30bdd8aca40b941bc5c5219851
unknown <dharwadkarjoanna@gmail.com>
2026-07-06 13:42:43 +0530

    Connect MongoDB Atlas using Mongoose

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Test Route
 app.get("/", (req, res) => {
     res.send("Food Donation API is running...");
 });
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
